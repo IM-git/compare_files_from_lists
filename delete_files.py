18cf5643837efe86a7b1223801dd442f48e5a689
IM94 <hereimenkov@gmail.com>
2022-08-14 18:55:03 +0300

    Added delete_files.py with methods for deleting the folders/files.

--- delete_files.py
+++ delete_files.py
@@ -1,6 +1,46 @@
+import os
+import shutil
+
+from tools.base_exeption import BaseExceptions
+
+
 class DeleteFiles:
     """
     Get a compare_files method text from compare_file.py
-    Delete the absence files taken from the list.
+    Delete the absence folders/files taken from the list.
     """
-    pass
+
+    def __init__(self, list_paths: str):
+        self.list_paths = list_paths
+
+    def iterating_through_values(self) -> None:
+        """Use loop for validate that is file/dir and
+        delete them."""
+        for value in self.list_paths.split('\n'):
+            try:
+                if os.path.isfile(value):
+                    self.delete_file(value)
+                elif os.path.isdir(value):
+                    self.delete_folder(value)
+                else:
+                    print(f"File: {value}, empty or isn't defined")
+            except BaseExceptions:
+                print('Something wrong! Check the files.')
+
+    @staticmethod
+    def delete_file(path_to_file: str) -> None:
+        os.remove(path_to_file)
+
+    @staticmethod
+    def delete_folder(path_to_file: str) -> None:
+        shutil.rmtree(path_to_file)
+
+
+if __name__ == '__main__':
+    from tools.read_file import ReadFile
+
+    PATH = 'menu.json'
+    DATA = ReadFile.read_json_file(PATH)
+    LIST_MISSING_FILES = ReadFile.read_file(DATA["new_file_with_parts"])
+
+    DeleteFiles(LIST_MISSING_FILES).iterating_through_values()
